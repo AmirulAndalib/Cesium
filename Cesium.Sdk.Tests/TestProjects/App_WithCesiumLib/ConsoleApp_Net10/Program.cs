87445a9ce573e197a107517b4fd134bb7724fa0f
Andrii Kurdiumov <kant2002@gmail.com>
2026-08-15 10:50:09 +0200

    Fix emitting target framework version attribute on assembly (#1004)
    * Fix emitting target framework version attribute on assembly
    * Added test for the publishing projects

---
+++ Cesium.Sdk.Tests/TestProjects/App_WithCesiumLib/ConsoleApp_Net10/Program.cs
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Cesium contributors <https://github.com/ForNeVeR/Cesium>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using CesiumLib;
+
+var result = new Foo();
+Console.WriteLine("Hello, " + result.x);
+Console.WriteLine("Hello world!");
